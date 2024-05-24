db78468d851f117717473d1f6b50843bd516d83a
amos <amossongor@gmail.com>
2024-05-24 17:11:03 -0400

    finished

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+return a + b;
+}
+
+function subtract(b, a) {
+return b - a;
+}
+
+function multiply(a, b) {
+return a * b;
+}
+
+function divide(a, b) {
+return a / b;
+}
+1 + 80;
+60 - 40; 
+2 * 3.4; 
+5.0 / 2.5;
+
+let n = 10.0
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1
+}
+
+function makeInt(n) {
+   return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
